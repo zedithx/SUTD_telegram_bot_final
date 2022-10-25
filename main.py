f33f3cc614b02e926188939164c72e6aabd1a48a
Yang Si Jun <aersijun@gmail.com>
2022-10-25 19:37:36 +0800

    test 2

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive.file',
          'https://www.googleapis.com/auth/drive']
 
-path = '/'
+path = '/app'
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/yangsijun/PycharmProjects/RootTelegramBot' + '/creds.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(path + '/creds.json', scope)
 
 client = gspread.authorize(creds)
 sheet = client.open('Echo@Cove').worksheet('TrialRun')
