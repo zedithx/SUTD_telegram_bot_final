ae11be1ab4865447c3e51d3465eee424b18af3d9
Yang Si Jun <aersijun@gmail.com>
2022-10-25 19:30:17 +0800

    first test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 from functools import wraps
 
+import gspread
 import telegram
+from oauth2client.service_account import ServiceAccountCredentials
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatAction
 from telegram.ext import CommandHandler, ConversationHandler, MessageHandler, \
     Updater, Filters, CallbackContext
@@ -17,181 +19,168 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-TOKEN = '5494892715:AAEieqoP3Ga4rJnwmopCeb3IXolWqvXPYjc'
-bot = telegram.Bot(token=TOKEN)
-CHOICE, POINTS, SET_GOAL, PLANS, ANSWER = range(5)
-track_points = 40400
-goal = 100000
+"""CONNECTING TO GOOGLE SHEETS"""
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive.file',
+         'https://www.googleapis.com/auth/drive']
 
-# Show that bot it "TYPING"
+path = '/'
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('RootTelegramBot'+ '/creds.json', scope)
+
+client = gspread.authorize(creds)
+sheet = client.open('Echo@Cove').worksheet('TrialRun')
+
+"""initialising variables/bot"""
+TOKEN = '5631416352:AAGNTPtHwtzZi3dEpywSRDLSzD7e9vJBYVA'
+bot = telegram.Bot(token=TOKEN)
+# Define no. of variables to be stored
+NAME, STUDENT_ID, MUSIC_THEME, CONFIRMATION, SUBMIT = range(5)
+# START --> NAME --> STUDENTID --> MUSIC THEME --> COMFIRMATION --> LOAD INTO GOOGLESHEETS --> SUBMIT
+userID_database = {}
+userID_savedindex = {}
+
 def send_typing_action(func):
+    """Wrapper to show that bot is typing"""
     @wraps(func)
     def command_func(update, context, *args, **kwargs):
         context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
         return func(update, context,  *args, **kwargs)
 
     return command_func
+
+# Explains details of the MusicFest event and ask if they would like to register
 @send_typing_action
 def start(update: Update, _: CallbackContext):
-    """Starts the conversation and asks what does user want to do."""
+    """Starts the conversation and asks whether the user would like to register for the MusicFest"""
     user = update.message.from_user
+    userID = str(update.message.chat_id)
+    if userID not in userID_database:
+        userID_database[userID] = []
     logger.info(f"{user.first_name} has started the bot")
+
+    update.message.reply_text(
+        "Hello. Welcome to the Echo@Cove 2022 Registration Bot \n"
+        "MusicFest is an event on 18 November where we have invited DJs to mix musics with 3 different themes\n"
+        "These 3 themes will be..."
+    )
+    bot.sendPhoto(update.message.chat_id, open("file name", 'rb'), caption='placeholder')
+    bot.sendPhoto(update.message.chat_id, open("file name", 'rb'), caption='placeholder')
+    bot.sendPhoto(update.message.chat_id, open("file name", 'rb'), caption='placeholder')
+    update.message.reply_text(
+        "We will be giving out wristbands based on the theme that u choose. \n"
+        "The main goal of this event is to allow everyone to bond with each other based on the music theme they enjoy"
+        "the most out of the 3 themes"
+        "Come along and sign up now to socialise with more people and just have a great time overall!"
+    )
     reply_keyboard = [["Yes", "No"]]
-
     update.message.reply_text(
-        "Hallo! I'm here to assist u with giving points to Si Jun \n"
-        "Send /cancel to stop talking to me.\n\n"
-        "Would u like to add friendship points for Si Jun???!!!",
+        "Would you like to register for MusicFest 2022?",
         reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Yes or No?"
-        ),
+            reply_keyboard)
     )
-    return CHOICE
-@send_typing_action
-def affection(update: Update, _: CallbackContext):
-    """Ask whether affection is needed"""
-    reply_keyboard = [["Yes", "No"]]
+    return NAME
 
-    update.message.reply_text(
-        "Are u missing Si Jun? \n"
-        "Do u need some affection from him?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Yes or No?"
-        ),
-    )
-    return ANSWER
+# Get Name
 @send_typing_action
-def affection_message(update: Update, _: CallbackContext):
-    """Shows photos and affection"""
+def name(update: Update, _: CallbackContext):
+    """Prompt user to enter name"""
     user = update.message.from_user
-    logger.info(f"{user.first_name} has chosen the affection option")
-    update.message.reply_text(
-        f"Hello I love u and miss u too!! Heres some cute photos to make u feel better!"
-    )
-    bot.sendPhoto(update.message.chat_id, open("Images/sijun.JPG", 'rb'), caption=
-    'Picture of me being stupid!')
-    bot.sendPhoto(update.message.chat_id, open("Images/tgt.JPG", 'rb'), caption=
-    'Picture of us tgt!')
-    bot.sendPhoto(update.message.chat_id, open("Images/sarah.JPG", 'rb'), caption=
-    'Picture of u being stupid!')
-    bot.sendPhoto(update.message.chat_id, open("Images/rhino.jpeg", 'rb'), caption=
-    'Picture of rhino!!!')
-    return ConversationHandler.END
-
-
-"""Progress choice"""
-@send_typing_action
-def progress(update:Update, _: CallbackContext):
-    "returns progress of friendship points"
-    user = update.message.from_user
-    ratio = int(track_points/goal*10)
-    bar = '█' * ratio
-    logger.info(f"{user.first_name} has checked the progress bar")
-    update.message.reply_text(
-        f"{bar} \n"
-        f"Points: {track_points}/{goal}\n"
-        "Goal: Have a nice meal with rhino looking at his relatives in the zoo!"
-    )
-    return ConversationHandler.END
-
-"""Set Goal choice"""
-@send_typing_action
-def choose_goal(update: Update, _: CallbackContext):
-
-    "Add points based on reply"
-    global goal
-    update.message.reply_text(
-        f"Please set a goal for Jun"
-    )
-    return SET_GOAL
-
-@send_typing_action
-def set_goal(update: Update, _: CallbackContext):
-    "Add points based on reply"
-    user = update.message.from_user
-    logger.info(f"{user.first_name} has set the goal to be {update.message.text} points")
-    global goal
-    if type(goal) != int:
+    if update.message.text == 'No':
+        logger.info(f"{user.first_name} has decided not to register")
+        userID = str(update.message.chat_id)
+        userID_database.pop(userID, None)
+        return ConversationHandler.END
+    elif update.message.text == 'Yes':
+        logger.info(f"{user.first_name} has indicated interest in registering")
         update.message.reply_text(
-            f"{goal} is not a number!"
+            'Please enter your full name.', reply_markup=ReplyKeyboardRemove())
+        return STUDENT_ID
+
+
+# Get Student Id
+@send_typing_action
+def student_id(update: Update, _: CallbackContext):
+    """Prompt user to enter Student ID"""
+    userID = str(update.message.chat_id)
+    userID_database[userID].append(update.message.text)
+    update.message.reply_text(
+            'Now please enter your Student ID.', reply_markup=ReplyKeyboardRemove())
+    return MUSIC_THEME
+
+# Get favourite music theme
+@send_typing_action
+def music_theme(update: Update, _: CallbackContext):
+    """Prompt user to choose favourite music theme"""
+    userID = str(update.message.chat_id)
+    userID_database[userID].append(update.message.text)
+    reply_keyboard = [['1', '2', '3']]
+    update.message.reply_text(
+        'Now please choose your favourite theme out of the 3. \n'
+        '1: placeholder \n'
+        '2: placeholder \n'
+        '3: placeholder \n',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard))
+    return CONFIRMATION
+
+@send_typing_action
+def confirmation(update: Update, _: CallbackContext):
+    """Prompt user to confirm his details after entering all his particulars"""
+    userID = str(update.message.chat_id)
+    update.message.reply_text('Please check your details before submitting.\n')
+    update.message.reply_text(
+        'Name: ' + userID_database[userID][0] + '\n'
+        'Student ID: ' + userID_database[userID][1] + '\n'
+        'Favourite Music Theme' + userID_database[userID][2])
+    reply_keyboard = [['Yes', 'No']]
+    update.message.reply_text('Are the details that you entered correct? \n \n'
+                              'Enter Yes or No.',
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return SUBMIT
+
+
+@send_typing_action
+def submit(update: Update, _:CallbackContext):
+    """Store details in savedindex dict as well as google sheets after user has confirmed details are correct"""
+    if update.message.text.lower() == 'yes':
+        print(userID_database)
+        userID = str(update.message.chat_id)
+        # Program to add to GOOGLE SHEETS HERE!
+        Name = userID_database[userID][0]
+        StudentID= userID_database[userID][1]
+        MusicTheme = userID_database[userID][2]
+        client = gspread.authorize(creds)
+        sheet = client.open('placeholder').worksheet('placeholder')
+        data = sheet.get_all_records()
+        row_to_insert = [Name, StudentID, MusicTheme]
+        userID_savedindex[userID] = len(data) + 2
+        sheet.insert_row(row_to_insert, len(data) + 2)
+        update.message.reply_text(
+            '<b>Registration completed!</b> \n'
+            'We hope you will have fun in this event!\n'
+            'We will see you on the 17th of October!'
         )
         return ConversationHandler.END
-    goal = int(update.message.text)
-    update.message.reply_text(
-        f"Thats quite high but okay FKS. Goal shall be {goal} now:<"
-    )
-    return ConversationHandler.END
-
-
-"""Set Plans Choice"""
-@send_typing_action
-def set_plans(update: Update, _: CallbackContext):
-    """Set plans and notify Si Jun"""
-    reply_keyboard = [["Mon", "Tues", "Wed", "Thurs", "Fri", "Saturday", "Sunday"]]
-    user = update.message.from_user
-    logger.info(f"{user.first_name} is setting plans")
-    update.message.reply_text(
-        "Wed: 12pm onwards to night \n"
-        "Thurs: Whole day +- gym",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="How many points?"
-        ),
-    )
-    return PLANS
-@send_typing_action
-def confirm_plans(update: Update, _: CallbackContext):
-    """Check if plans are available"""
-    user = update.message.from_user
-    if update.message.text.lower() == 'wed' or update.message.text.lower() == 'thurs':
-        update.message.reply_text(
-            "Ur plans have been made with Si Jun. Si Jun will confirm with u on text in abit on the timing!"
-        )
-        logger.info(f"{user.first_name} has set a date with you on the {update.message.text}")
+    # if information is incorrect, remove from the database dictionary and end conversation
     else:
         update.message.reply_text(
-            "Please check the available dates again. It appears Si Jun is not free during that day :<"
-        )
-    return ConversationHandler.END
-
-@send_typing_action
-def choice(update: Update, _: CallbackContext):
-    "Based on Choice, exit programme or gets points inputted"
-    reply_keyboard = [["400", "800", "1200", "1600", "2000", "5000"]]
-    user = update.message.from_user
-    logger.info(f"{user.first_name} chose {update.message.text}")
-    if update.message.text.lower() == 'no':
-        update.message.reply_text(
-            "Then u type /start for what bij?!!!", reply_markup=ReplyKeyboardRemove()
-        )
+            'Registration is cancelled. \n'
+            'Please start the bot again and enter the right particulars')
+        userID = str(update.message.chat_id)
+        userID_database.pop(userID, None)
         return ConversationHandler.END
-    else:
-        update.message.reply_text(
-            "How many points should Mr Yang get today?!!!",
-            reply_markup=ReplyKeyboardMarkup(
-                reply_keyboard, one_time_keyboard=True, input_field_placeholder="How many points?"
-            ),
-        )
-    return POINTS
-@send_typing_action
-def points(update: Update, _: CallbackContext):
-
-    "Add points based on reply"
-    user = update.message.from_user
-    logger.info(f"{user.first_name} has given {update.message.text} points")
-    global track_points
-    track_points += int(update.message.text)
-    update.message.reply_text(
-        f"Jun now has {track_points} points. FKS"
-    )
-    return ConversationHandler.END
 @send_typing_action
 def cancel(update: Update, _: CallbackContext):
     """Cancels and ends the conversation."""
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
-        "I guess Si Jun doesn't deserve points today:<", reply_markup=ReplyKeyboardRemove()
+        "We hope that you will eventually join this music festival!", reply_markup=ReplyKeyboardRemove()
     )
+    # remove from the database dictionary
+    userID = str(update.message.chat_id)
+    userID_database.pop(userID, None)
 
     return ConversationHandler.END
 
@@ -199,60 +188,26 @@ def cancel(update: Update, _: CallbackContext):
 
 if __name__ == '__main__':
     updater = Updater(TOKEN, use_context=True)
-    # application = ApplicationBuilder().token('5494892715:AAEieqoP3Ga4rJnwmopCeb3IXolWqvXPYjc').build()
     dispatcher = updater.dispatcher
 
 
     start_conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            CHOICE: [MessageHandler(Filters.regex("^(?:Yes|No)$"), choice)],
-            POINTS: [MessageHandler(Filters.regex("^400|800|1200|1600|2000|5000$"), points)]
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
-
-    goal_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("goal", choose_goal)],
-        states={
-            SET_GOAL: [MessageHandler(Filters.regex("(.*?)"), set_goal)],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
-
-    affection_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("affection", affection)],
-        states={
-            ANSWER: [MessageHandler(Filters.regex("^(?:Yes|No)$"), affection_message)],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
-
-    progress_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("progress", progress)],
-        states={},
-        fallbacks=[CommandHandler("cancel", cancel)]
-    )
-
-    plans_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("plan", set_plans)],
-        states={
-            PLANS: [MessageHandler(Filters.regex("^(?:Mon|Tues|Wed|Thurs|Fri|Sat|Sun)$"), confirm_plans)]
+            NAME: [MessageHandler(Filters.text, name)],
+            STUDENT_ID: [MessageHandler(Filters.text, name)],
+            MUSIC_THEME: [MessageHandler(Filters.regex('^[1-5]$'), music_theme)],
+            SUBMIT: [MessageHandler(Filters.regex('(?i)^(yes|no)$'), submit)]
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
     dispatcher.add_handler(start_conv_handler)
-    dispatcher.add_handler(progress_conv_handler)
-    dispatcher.add_handler(goal_conv_handler)
-    dispatcher.add_handler(plans_conv_handler)
-    dispatcher.add_handler(affection_conv_handler)
-    print('check')
+    dispatcher.add_handler(CommandHandler('cancel', cancel))
 
     test = updater.start_webhook(listen="0.0.0.0",
                                  port=PORT,
                                  url_path=TOKEN,
-                                 webhook_url='https://immense-anchorage-33732.herokuapp.com/' + TOKEN)
+                                 webhook_url='' + TOKEN)
 
-    print(test)
     updater.idle()
